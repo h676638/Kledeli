0214fc3229f1eb8eb056fd1efe68487605118121
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-24 15:23:37 +0200

    topnav ting

--- navbar-2.js
+++ navbar-2.js
@@ -1,10 +1,10 @@
 
 let x1 = "<div class=\"topnav\">";
-let x2 = "    <a class=\"topnav-logo\"href=\"../../index.html\">Kledeli</a>";
-let x3 = "    <a href=\"../../omKledeli.html\">Om Kledeli</a>";
-let x4 = "    <a href=\"../../plagg.html\">Klær</a>";
-let x5 = "    <a href=\"../../medlemskap.html\">Medlemskap</a>";
-let x6 = "    <a href=\"../../lokasjoner.html\">Lokasjoner</a>";
+let x2 = "    <a class=\"topnav-logo\"href=\"../../index.html\">&zwnj; </a>";
+let x3 = "    <a class=\"topnav-not-logo\" href=\"../../plagg.html\">Klær</a>";
+let x4 = "    <a class=\"topnav-not-logo\" href=\"../../medlemskap.html\">Medlemskap</a>";
+let x5 = "    <a class=\"topnav-not-logo\" href=\"../../lokasjoner.html\">Lokasjoner</a>";
+let x6 = "    <a class=\"topnav-not-logo\" href=\"../../omKledeli.html\">Om Kledeli</a>";
 let x7 = "</div>";
 const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7;
 document.write(navbar);
--- navbar.js
+++ navbar.js
@@ -1,10 +1,11 @@
 
 let x1 = "<div class=\"topnav\">";
-let x2 = "    <a class =\"topnav-logo\"href=\"index.html\">Kledeli</a>";
-let x3 = "    <a href=\"omKledeli.html\">Om Kledeli</a>";
-let x4 = "    <a href=\"plagg.html\">Klær</a>";
-let x5 = "    <a href=\"medlemskap.html\">Medlemskap</a>";
-let x6 = "    <a href=\"lokasjoner.html\">Lokasjoner</a>";
+let x2 = "    <a class =\"topnav-logo\" href=\"index.html\"> &zwnj; </a>";
+let x3 = "    <a class=\"topnav-not-logo\" href=\"plagg.html\">KLÆR</a>";
+let x4 = "    <a class=\"topnav-not-logo\" href=\"medlemskap.html\">MEDLEMSKAP</a>";
+let x5 = "    <a class=\"topnav-not-logo\" href=\"lokasjoner.html\">LOKASJONER</a>";
+let x6 = "    <a class=\"topnav-not-logo\" href=\"omKledeli.html\">OM KLEDELI</a>";
 let x7 = "</div>";
-const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7;
+let x8 = "<br>";
+const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8;
 document.write(navbar);
