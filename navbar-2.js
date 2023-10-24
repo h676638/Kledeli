d3470d199b359bfdf3e621c3d308a1ce6e015484
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-24 13:37:18 +0200

    Laget navbar generator script

---
+++ navbar-2.js
@@ -0,0 +1,10 @@
+
+let x1 = "<div class=\"topnav\">";
+let x2 = "    <a href=\"../../index.html\">Kledeli</a>";
+let x3 = "    <a href=\"../../omKledeli.html\">Om Kledeli</a>";
+let x4 = "    <a href=\"../../plagg.html\">Klær</a>";
+let x5 = "    <a href=\"../../medlemskap.html\">Medlemskap</a>";
+let x6 = "    <a href=\"../../lokasjoner.html\">Lokasjoner</a>";
+let x7 = "</div>";
+const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7;
+document.write(navbar);
