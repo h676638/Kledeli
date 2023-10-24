0913ba3266cefae3550776e87bd667273194eb4b
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-24 14:00:09 +0200

    ting

--- navbar-2.js
+++ navbar-2.js
@@ -1,6 +1,6 @@
 
 let x1 = "<div class=\"topnav\">";
-let x2 = "    <a href=\"../../index.html\">Kledeli</a>";
+let x2 = "    <a class=\"topnav-logo\"href=\"../../index.html\">Kledeli</a>";
 let x3 = "    <a href=\"../../omKledeli.html\">Om Kledeli</a>";
 let x4 = "    <a href=\"../../plagg.html\">Klær</a>";
 let x5 = "    <a href=\"../../medlemskap.html\">Medlemskap</a>";
