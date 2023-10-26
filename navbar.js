4da0406aba8e4e037719d2c923957d16898bc34b
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-26 12:51:49 +0200

    added link to user in navbar

--- navbar.js
+++ navbar.js
@@ -5,18 +5,19 @@ let x3 = "    <a class=\"topnav-not-logo\" href=\"plagg.html\">KLÆR</a>";
 let x4 = "    <a class=\"topnav-not-logo\" href=\"medlemskap.html\">MEDLEMSKAP</a>";
 let x5 = "    <a class=\"topnav-not-logo\" href=\"lokasjoner.html\">LOKASJONER</a>";
 let x6 = "    <a class=\"topnav-not-logo\" href=\"omKledeli.html\">OM KLEDELI</a>";
-let x7 = "    <a id=\"topnav-icon\"class=\"topnav-icon\">&#9776;</a>"
-let x8 = "</div>";
-let x9 = "<br>";
-let x10 = "<div id=\"topnav-space\"> </div>";
-const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+let x7 = "    <a class=\"topnav-user\" href=\"logginn.html\">&zwnj;</a>";
+let x8 = "    <a id=\"topnav-icon\"class=\"topnav-icon\">&#9776;</a>"
+let x9 = "</div>";
+let x10 = "<br>";
+let x11 = "<div id=\"topnav-space\"> </div>";
+const navbar = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11;
 document.write(navbar);
 function myFunction() {
     let navbar = document.getElementById("myTopnav");
     let topnavSpace = document.getElementById("topnav-space");
     if (navbar.className === "topnav") {
         navbar.className += " responsive";
-        topnavSpace.style.marginBottom = "145px";
+        topnavSpace.style.marginBottom = "165px";
     } 
     else {
         navbar.className = "topnav";
