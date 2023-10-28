db939d821d9fedc12549f5079b6ffcf2ac6f65e0
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-28 12:29:01 +0200

    Begynnte på plagg.html

---
+++ plagg.js
@@ -0,0 +1,16 @@
+const tmpChildDivs = document.getElementsByClassName("plagg-kategorier");
+const childDivs = Array.from(tmpChildDivs); 
+childDivs.forEach((div) => {
+    let details = div.querySelector("details");
+    let children = details.querySelectorAll("a");
+    console.log(children);
+    details.addEventListener("toggle", () => {
+        if (details.open) {
+            let height = children.length * 34.4 + 250;
+            div.style.height = height + "px";
+        }
+        else {
+            div.style.height = "250px";
+        }
+    })
+});
