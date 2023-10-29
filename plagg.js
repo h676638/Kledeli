42e1245edfdb52319693833f1a365d3d4d798a88
h676638 <142889364+h676638@users.noreply.github.com>
2023-10-29 12:19:37 +0100

    bug fiks og justering på plagg-undersider

--- plagg.js
+++ plagg.js
@@ -3,7 +3,6 @@ const childDivs = Array.from(tmpChildDivs);
 childDivs.forEach((div) => {
     let details = div.querySelector("details");
     let children = details.querySelectorAll("a");
-    console.log(children);
     details.addEventListener("toggle", () => {
         if (details.open) {
             let height = children.length * 34.4 + 250;
@@ -12,10 +11,11 @@ childDivs.forEach((div) => {
         else {
             div.style.height = "250px";
         }
-        event.stopPropagation();
     })
     div.addEventListener("click", (event) => {
-        details.toggleAttribute("open");
-        event.preventDefault();
+        if (event.target.localName != "p") {
+            details.toggleAttribute("open");
+            event.preventDefault();
+        }
     })
 });
